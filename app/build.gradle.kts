0e2920a661bc3d7d8bfd7a8387394a265cf3400d
tychomagnetic <alex.crosby@gmail.com>
2026-08-23 17:08:14 +0100

    Release v0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "io.github.tychomagnetic.metterweather"
     minSdk = 26
     targetSdk = 36
-    versionCode = 6
-    versionName = "0.6"
+    versionCode = 7
+    versionName = "0.7"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
